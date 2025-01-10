e3525cd2bae066d52350a5fcd3a6dbb1fabdbdd8
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-01-10 19:29:08 +0100

    Buttons working

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ let score = 0;
 const questionContainer = document.getElementById("questionContainer");
 const startBtn = document.getElementById("startBtn");
 const nextBtn = document.getElementById("nextBtn");
+const answersContainer = document.getElementById("answersContainer");
+let progress = 0;
 
 // FETCHING API, RETURNING ARRAY OF QUESTION OBJECTS
 export async function fetchApi() {
@@ -26,7 +28,7 @@ export async function fetchApi() {
   }
 }
 
-// fetchApi();
+fetchApi();
 
 // START THE GAME WHEN START BUTTON IS CLICKED
 startBtn.addEventListener("click", () => {
@@ -38,16 +40,64 @@ function startGame() {
   questionContainer.classList.remove("hide");
   nextBtn.classList.remove("hide");
   generateQuestion();
-
 }
 
-// SHOW QUESTIONS & ANSWERS
-
+// SHOW QUESTION & GENERATE A BUTTON FOR EACH ANSWER
 function generateQuestion() {
+  document.getElementById("question").innerText =
     questionArray[questionNumber].question;
+  // if question includes "&quot;" change to '"'
+  // if question includes "&#039" change to "'"
 
+  const answers = [
+    ...questionArray[questionNumber].incorrect_answers,
+    questionArray[questionNumber].correct_answer,
+  ];
+  answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.classList.add("answerBtn");
+    button.textContent = answer;
+    answersContainer.appendChild(button);
+    button.addEventListener("click", (e) => {
+      checkAnswer(e);
+    });
+  });
+  // randomize answers positions, shuffle. answers.sort by random index
+  // click only one button at the time
 }
 
+// CHECK IF CLICKED ANSWER IS CORRECT
+function checkAnswer(e) {
+  const selectedButton = e.target;
+  const selectedAnswer = selectedButton.textContent;
+  const correctAnswer = questionArray[questionNumber].correct_answer;
+  if (selectedAnswer === correctAnswer) {
+    selectedButton.classList.add("correct-button");
+    score++;
+  } else {
+    selectedButton.classList.add("wrong-button");
+  }
+}
 
+// NEXT BUTTON CLICK, UPDATE INDEX, UPDATE PROGRESS BAR, GENERATE NEW QUESTION, ETC.
+nextBtn.addEventListener("click", () => {
+  answersContainer.innerHTML = "";
+  questionNumber++;
+  progress += 10;
+  updateProgressBar();
+  generateQuestion();
+});
 
-// ANSWER CLICKED
+function updateProgressBar() {
+    progressBar.style.background = `linear-gradient(to right, var(--contrast) ${progress}%, transparent ${progress}%, transparent 100%)`;
+  }
+
+// SHOW SCORE PAGE
+
+//   when questionArray length = 10
+//   show score (instead of question)
+//   show restartBtn
+//   hide everything else
+// document.getElementById("restartBtn").addEventListener("click", () => {
+//     startGame();
+//   });
